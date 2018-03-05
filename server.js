ce72f33e7dac775ed9708993e9b639e99ba47203
withsumanth <withsumanth@gmail.com>
2018-03-04 23:53:35 -0500

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
+app.listen(process.env.PORT || 8080);
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -22,4 +23,3 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.listen(process.env.PORT || 8080);
