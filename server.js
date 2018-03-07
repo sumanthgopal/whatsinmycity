8df390c866ebc77a36a8ee0f9e60cde082aa774a
withsumanth <withsumanth@gmail.com>
2018-03-07 15:22:54 -0500

    updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var path=require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.listen(process.env.PORT || 8080);
