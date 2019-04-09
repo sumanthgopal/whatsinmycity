c0411db83b7c84e5ed2baafc6f4029bc1712b0bd
withsumanth <33943351+withsumanth@users.noreply.github.com>
2019-04-09 00:05:57 -0400

    Update user-service.ts

--- src/app/user/user-service.ts
+++ src/app/user/user-service.ts
@@ -16,11 +16,11 @@ export class UserService {
   }
   onSignup(details) {
     this.checkLogout = false;
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/insert.php/', details, this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/insert.php/', details, this.options)
       .map(() => '');
   }
   getUsers() {
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/select.php/','' , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/select.php/','' , this.options)
       .map( res => res.json());
   }
 
@@ -33,47 +33,47 @@ export class UserService {
   }
 
   selectData(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'restaurant', 'city': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'restaurant', 'city': values} , this.options)
         .map( res => res.json());
   }
 
   selectDataEvents(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/selectEvents.php/', {'searchValue': 'events', 'city': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/selectEvents.php/', {'searchValue': 'events', 'city': values} , this.options)
       .map( res => res.json());
   }
 
   selectDataPlaceofInt(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/selectPlaceOfInt.php/', {'searchValue': 'placesofinterest', 'city': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/selectPlaceOfInt.php/', {'searchValue': 'placesofinterest', 'city': values} , this.options)
       .map( res => res.json());
   }
 
   selectDataNightLife(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'nightlife', 'city': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'nightlife', 'city': values} , this.options)
       .map( res => res.json());
   }
 
   selectDataShopping(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'shopping', 'city': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/selectRestaurants.php/', {'searchValue': 'shopping', 'city': values} , this.options)
       .map( res => res.json());
   }
 
   contactUs(name ,email , msg){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/contactus.php/', {'name':name,'email':email,'msg':msg} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/contactus.php/', {'name':name,'email':email,'msg':msg} , this.options)
       .map(() => '');
   }
 
   careerRequest(name ,email ,phone, msg){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/career.php/', {'name':name,'email':email,'phone':phone,'msg':msg} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/career.php/', {'name':name,'email':email,'phone':phone,'msg':msg} , this.options)
       .map(() => '');
   }
 
   getSelectedUsers(values){
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/insertwithdata.php/', {'mail': values} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/insertwithdata.php/', {'mail': values} , this.options)
       .map( res => res.json());
   }
 
   updateReview(values,rating) {
-    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.1d35.starter-us-east-1.openshiftapps.com/updatereview.php/',{'rev': values,'rat':rating} , this.options)
+    return this._http.post('https://whatsinmycityphp-whatsinmycityphp.7e14.starter-us-west-2.openshiftapps.com/updatereview.php/',{'rev': values,'rat':rating} , this.options)
       .map(() => '');
   }
 
